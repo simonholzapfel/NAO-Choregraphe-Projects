997aca53f7fc1f6cdff8837187ff89ce93062ac2
Marc_Laptop <k.marc@a1.net>
2019-03-26 15:25:02 +0100

    update

--- Waiter/translations/translation_de_DE.ts
+++ Waiter/translations/translation_de_DE.ts
@@ -708,6 +708,20 @@
         </message>
     </context>
     <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hallo</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ihr Betrag zu bezahlen lautet</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ihr Betrag zu bezahlen lautet</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/TellOrder/Say</name>
         <message>
             <location filename="behavior_1/behavior.xar" line="0"/>
