f4377aed72dbf61aef18312fbc905056099545a7
Marc_Laptop <k.marc@a1.net>
2019-03-29 15:21:30 +0100

    update

--- Waiter/translations/translation_de_DE.ts
+++ Waiter/translations/translation_de_DE.ts
@@ -681,6 +681,15 @@
         </message>
     </context>
     <context>
+        <name>behavior_1/behavior.xar:/GiveBestellung (1)/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Bitte entnehmen Sie ihre Bestellung</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Bitte entnehmen Sie ihre Bestellung</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/GiveBestellung/Say</name>
         <message>
             <location filename="behavior_1/behavior.xar" line="0"/>
