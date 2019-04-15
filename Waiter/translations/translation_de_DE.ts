b168127e5adf584bc725a37287d37b8aeffb1120
robertFreiseisen <43356031+robertFreiseisen@users.noreply.github.com>
2019-04-15 13:36:40 +0200

    update

--- Waiter/translations/translation_de_DE.ts
+++ Waiter/translations/translation_de_DE.ts
@@ -905,6 +905,15 @@
         </message>
     </context>
     <context>
+        <name>behavior_1/behavior.xar:/Program itself/GiveBestellung (2)/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Bitte entnehmen Sie ihre Bestellung</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Bitte entnehmen Sie ihre Bestellung</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/Program itself/GiveBestellung/Say</name>
         <message>
             <location filename="behavior_1/behavior.xar" line="0"/>
