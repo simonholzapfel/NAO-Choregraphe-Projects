5258a49c60c8edab1789a09a191e2ad99c679e7b
Marc_Laptop <k.marc@a1.net>
2019-04-26 00:13:33 +0200

    update

--- Waiter/translations/translation_de_DE.ts
+++ Waiter/translations/translation_de_DE.ts
@@ -4023,6 +4023,15 @@
         </message>
     </context>
     <context>
+        <name>behavior_1/behavior.xar:/SaveFerdinandFace/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hallo</source>
+            <comment>Text</comment>
+            <translation>Hallo</translation>
+        </message>
+    </context>
+    <context>
         <name>behavior_1/behavior.xar:/Say</name>
         <message>
             <source>Hallo</source>
