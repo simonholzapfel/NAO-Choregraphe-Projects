8fae3f247cfd9c8e2902c9f73e0202fde3641008
Simon Holzapfel <38250977+simonshs@users.noreply.github.com>
2019-08-27 13:25:41 +0200

    last update for wedding

--- Wedding2019/Wedding2019.pml
+++ Wedding2019/Wedding2019.pml
@@ -8,7 +8,7 @@
     <Resources />
     <Topics />
     <IgnoredPaths />
-    <Translations auto-fill="en_US">
-        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    <Translations auto-fill="de_DE">
+        <Translation name="translation_de_DE" src="translations/translation_de_DE.ts" language="de_DE" />
     </Translations>
 </Package>
---
+++ Wedding2019/translations/translation_de_DE.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello</translation>
+        </message>
+    </context>
+</TS>
